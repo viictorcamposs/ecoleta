9f6cf11cd5aa4e34f3cec4d5b543c8651b0e0899
Victor Campos <campos.viictor@gmail.com>
2020-06-04 14:07:32 -0300

    Configurando arquivos para trabalhar com NodeJS

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require ("express");
+const server = express ();
+
+// configurar pasta pública
+  server.use (express.static ("public"))
+
+// utilizando template engine 
+  const nunjucks = require ("nunjucks")
+  nunjucks.configure ("src/views", {
+    express: server,
+    noCache: true
+  })
+
+
+//configurar caminhos da minha aplicação
+  // req: requisição | res: resposta
+
+  // página inicial
+    server.get ("/", function (req, res) {
+      return res.render ("index.html", { title: "Seu marketplace de coleta de resíduos"});
+    })
+
+  // página de cadastro
+    server.get ("/create-point", function (req, res) {
+      return res.render ("create-point.html");
+    })
+  
+  // página de resultados
+    server.get ("/search-results", function (req, res) {
+      res.sendFile (__dirname + "/views/search-results.html")
+    })
+
+// ligar o servidor
+  server.listen (3000)
